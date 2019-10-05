203f6f5f4c620964f8bf866c9ea8633719cd1d87
Charles DeLeo <flatironschool@Flatirons-Air.fios-router.home>
2019-10-05 15:46:03 -0400

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+    roll = rand(6)+1
+    puts "Guess a number between 1 and 6:"
+    user_input = gets.chomp
+    if user_input == roll.to_s
+        puts "You guessed the correct number!"
+    elsif user_input.downcase == "exit"
+        puts "Goodbye!"
+    else
+        puts "Sorry! The computer guessed #{roll}."
+    end
+end
